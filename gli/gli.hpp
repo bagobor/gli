86aafbbabd4f626b7ad7f2878dba139ab97f5172
Christophe Riccio <g.truc.creation@gmail.com>
2010-09-29 11:43:02 +0100

    Added version

--- gli/gli.hpp
+++ gli/gli.hpp
@@ -2,7 +2,7 @@
 // OpenGL Image Copyright (c) 2008 - 2010 G-Truc Creation (www.g-truc.net)
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Created : 2008-12-19
-// Updated : 2010-09-27
+// Updated : 2010-09-29
 // Licence : This source is under MIT License
 // File    : gli/gli.hpp
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -14,7 +14,13 @@
 #ifndef GLI_GLI_INCLUDED
 #define GLI_GLI_INCLUDED
 
-#include "./core/image.hpp"
+#define GLI_VERSION					30
+#define GLI_VERSION_MAJOR			0
+#define GLI_VERSION_MINOR			3
+#define GLI_VERSION_PATCH			0
+#define GLI_VERSION_REVISION		0
+
+#include "./core/texture.hpp"
 #include "./core/size.hpp"
 #include "./core/operation.hpp"
 #include "./core/generate_mipmaps.hpp"
