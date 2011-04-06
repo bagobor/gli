f38ef9f4d8d171bfa9aeb4bcc4e9c467fa96a944
Christophe Riccio <g.truc.creation@gmail.com>
2011-04-06 18:23:10 +0100

    Added cube map array files

--- gli/gli.hpp
+++ gli/gli.hpp
@@ -14,15 +14,16 @@
 #ifndef GLI_GLI_INCLUDED
 #define GLI_GLI_INCLUDED
 
-#define GLI_VERSION					30
+#define GLI_VERSION					31
 #define GLI_VERSION_MAJOR			0
 #define GLI_VERSION_MINOR			3
-#define GLI_VERSION_PATCH			0
-#define GLI_VERSION_REVISION		2
+#define GLI_VERSION_PATCH			1
+#define GLI_VERSION_REVISION		0
 
 #include "./core/texture2d.hpp"
 #include "./core/texture2d_array.hpp"
 #include "./core/texture_cube.hpp"
+#include "./core/texture_cube_array.hpp"
 #include "./core/size.hpp"
 #include "./core/operation.hpp"
 #include "./core/generate_mipmaps.hpp"
