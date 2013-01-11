9ea6f6ad53a8aff8ffb2905d4acf84efc012fb7b
Christophe Riccio <mail@g-truc.net>
2013-01-11 11:44:42 +0000

    Updated interface from texture1D and texture1DArray

--- gli/core/texture1d.hpp
+++ gli/core/texture1d.hpp
@@ -1,11 +1,30 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// OpenGL Image Copyright (c) 2008 - 2011 G-Truc Creation (www.g-truc.net)
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Created : 2012-06-25
-// Updated : 2012-10-16
-// Licence : This source is under MIT License
-// File    : gli/core/texture1d.hpp
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+/// OpenGL Image (gli.g-truc.net)
+///
+/// Copyright (c) 2008 - 2013 G-Truc Creation (www.g-truc.net)
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+///
+/// @ref core
+/// @file gli/core/texture1d.hpp
+/// @date 2012-06-25 / 2013-01-11
+/// @author Christophe Riccio
+///////////////////////////////////////////////////////////////////////////////////
 
 #ifndef GLI_CORE_TEXTURE1D_INCLUDED 
 #define GLI_CORE_TEXTURE1D_INCLUDED
@@ -14,51 +33,51 @@
 
 namespace gli
 {
-	//template <template <typename> class mem>
 	class texture1D
 	{
 	public:
-		typedef gli::detail::storage::dimensions1_type dimensions_type;
-		typedef gli::detail::storage::texcoord1_type texcoord_type;
-		typedef gli::detail::storage::size_type size_type;
-		typedef gli::format format_type;
-		typedef glm::byte data_type;
+		typedef detail::storage::dimensions1_type dimensions_type;
+		typedef detail::storage::texcoord1_type texcoord_type;
+		typedef detail::storage::size_type size_type;
+		typedef format format_type;
 
 	public:
 		texture1D();
 
+		/// Allocate a new storage constructor
 		explicit texture1D(
-			size_type const & Levels,
-			format_type const & InternalFormat,
-			dimensions_type const & Dimensions);
-/*
-		template <typename genType>
+            size_type const & Levels,
+            format_type const & Format,
+            dimensions_type const & Dimensions);
+        
+		/// Reference an exiting storage constructor
 		explicit texture1D(
-			size_type const & Levels,
-			format_type const & InternalFormat, 
-			dimensions_type const & Dimensions,
-			genType const & Texel);
-*/
+            format_type const & Format,
+            shared_ptr<detail::storage> const & Storage,
+            detail::view const & View);
+        
 		~texture1D();
 
-		image & operator[] (
-			size_type const & Level);
-		image const & operator[] (
-			size_type const & Level) const;
+		image const & operator[] (size_type const & Level) const;
 
 		bool empty() const;
-		format_type format() const;
+		size_type size() const;
 		dimensions_type dimensions() const;
 		size_type levels() const;
-		size_type size() const;
-		data_type * data();
-		data_type const * const data() const;
+		format_type format() const;
+        
+		void * data();
+		void const * const data() const;
+        
+		template <typename genType>
+		genType * data();
+		template <typename genType>
+		genType const * const data() const;
 
 	private:
-		detail::storage Storage;
-		std::vector<image> Images;
-		format_type const Format;
-		size_type Offset;
+		shared_ptr<detail::storage> Storage;
+		detail::view View;
+		format_type Format;
 	};
 
 }//namespace gli
