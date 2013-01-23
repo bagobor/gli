be6a9044354f83330744b6732df8abf8ca593369
Christophe Riccio <mail@g-truc.net>
2013-01-23 22:03:11 +0100

    Fixed texture 3d tests

--- test/core/core_texture_3d.cpp
+++ test/core/core_texture_3d.cpp
@@ -53,42 +53,34 @@ int test_texture3d_query()
 		gli::RGBA8U,
 		gli::texture3D::dimensions_type(2));
 
-	Error += Texture.size() == sizeof(glm::u8vec4) * 5 ? 0 : 1;
+	Error += Texture.size() == sizeof(glm::u8vec4) * 9 ? 0 : 1;
 	Error += Texture.format() == gli::RGBA8U ? 0 : 1;
 	Error += Texture.levels() == 2 ? 0 : 1;
 	Error += !Texture.empty() ? 0 : 1;
 	Error += Texture.dimensions().x == 2 ? 0 : 1;
 	Error += Texture.dimensions().y == 2 ? 0 : 1;
+	Error += Texture.dimensions().z == 2 ? 0 : 1;
 
 	return Error;
 }
 
-int test_texture3d_image_access()
+int test_texture3d_access()
 {
 	int Error(0);
 
 	{
 		glm::u8vec4 const Orange(255, 127, 0, 255);
 
-		gli::image Image0(gli::image::dimensions_type(2, 2, 1), sizeof(glm::u8vec4), gli::image::dimensions_type(1));
-		for(std::size_t i = 0; i < Image0.size(); ++i)
-			*(Image0.data<glm::byte>() + i) = glm::byte(i);
-
-		gli::image Image1(gli::image::dimensions_type(1, 1, 1), sizeof(glm::u8vec4), gli::image::dimensions_type(1));
-		for(std::size_t i = 0; i < Image1.size(); ++i)
-			*(Image1.data<glm::byte>() + i) = glm::byte(i + 100);
+		gli::image Image(
+			gli::RGBA8U, 
+			gli::image::dimensions_type(2));
 
 		gli::texture3D Texture(
-			gli::texture3D::size_type(2),
+			gli::texture3D::size_type(1),
 			gli::RGBA8U,
 			gli::texture3D::dimensions_type(2));
 
-		/// TODO copy function
-		/// Texture[0] = Image0;
-		/// Texture[1] = Image1;
-
-		/// Error += Texture[0] == Image0 ? 0 : 1;
-		/// Error += Texture[1] == Image1 ? 0 : 1;
+		Error += Image.size() == Texture.size() ? 0 : 1;
 	}
 
 	{
@@ -104,7 +96,7 @@ int test_texture3d_image_access()
 		std::size_t Size0 = Image0.size();
 		std::size_t Size1 = Image1.size();
 
-		Error += Size0 == sizeof(glm::u8vec4) * 4 ? 0 : 1;
+		Error += Size0 == sizeof(glm::u8vec4) * 8 ? 0 : 1;
 		Error += Size1 == sizeof(glm::u8vec4) * 1 ? 0 : 1;
 
 		*Image0.data<glm::u8vec4>() = glm::u8vec4(255, 127, 0, 255);
@@ -114,7 +106,7 @@ int test_texture3d_image_access()
 		glm::u8vec4 * PointerB = Image1.data<glm::u8vec4>();
 
 		glm::u8vec4 * Pointer0 = Texture.data<glm::u8vec4>() + 0;
-		glm::u8vec4 * Pointer1 = Texture.data<glm::u8vec4>() + 4;
+		glm::u8vec4 * Pointer1 = Texture.data<glm::u8vec4>() + 8;
 
 		Error += PointerA == Pointer0 ? 0 : 1;
 		Error += PointerB == Pointer1 ? 0 : 1;
@@ -129,35 +121,10 @@ int test_texture3d_image_access()
 		Error += glm::all(glm::equal(Color1, glm::u8vec4(0, 127, 255, 255))) ? 0 : 1;
 	}
 
-	{
-		gli::texture3D Texture(
-			gli::texture3D::size_type(1),
-			gli::RGBA8U,
-			gli::texture3D::dimensions_type(2));
-
-		std::size_t SizeA = Texture.size();
-		Error += SizeA == sizeof(glm::u8vec4) * 4 ? 0 : 1;
-
-		gli::image Image0 = Texture[0];
-		
-		std::size_t Size0 = Image0.size();
-		Error += Size0 == sizeof(glm::u8vec4) * 4 ? 0 : 1;
-
-		*Image0.data<glm::u8vec4>() = glm::u8vec4(255, 127, 0, 255);
-
-		glm::u8vec4 * PointerA = Image0.data<glm::u8vec4>();
-		glm::u8vec4 * Pointer0 = Texture.data<glm::u8vec4>() + 0;
-		Error += PointerA == Pointer0 ? 0 : 1;
-
-		glm::u8vec4 ColorA = *PointerA;
-		glm::u8vec4 Color0 = *Pointer0;
-		Error += glm::all(glm::equal(Color0, glm::u8vec4(255, 127, 0, 255))) ? 0 : 1;
-	}
-
 	return Error;
 }
 
-int test_texture3d_image_size()
+int test_texture3d_size()
 {
 	int Error(0);
 
@@ -178,12 +145,12 @@ int test_texture3d_image_size()
 	};
 
 	std::vector<test> Tests;
-	Tests.push_back(test(gli::RGBA8U, gli::texture3D::dimensions_type(4), 64));
-	Tests.push_back(test(gli::R8U, gli::texture3D::dimensions_type(4), 16));
-	Tests.push_back(test(gli::DXT1, gli::texture3D::dimensions_type(4), 8));
-	Tests.push_back(test(gli::DXT1, gli::texture3D::dimensions_type(2), 8));
-	Tests.push_back(test(gli::DXT1, gli::texture3D::dimensions_type(1), 8));
-	Tests.push_back(test(gli::DXT5, gli::texture3D::dimensions_type(4), 16));
+	Tests.push_back(test(gli::RGBA8U, gli::texture3D::dimensions_type(4), 256));
+	Tests.push_back(test(gli::R8U, gli::texture3D::dimensions_type(4), 64));
+	Tests.push_back(test(gli::DXT1, gli::texture3D::dimensions_type(4), 32));
+	Tests.push_back(test(gli::DXT1, gli::texture3D::dimensions_type(2), 32));
+	Tests.push_back(test(gli::DXT1, gli::texture3D::dimensions_type(1), 32));
+	Tests.push_back(test(gli::DXT5, gli::texture3D::dimensions_type(4), 64));
 
 	for(std::size_t i = 0; i < Tests.size(); ++i)
 	{
@@ -216,10 +183,10 @@ int main()
 {
 	int Error(0);
 
-	Error += test_texture3d_image_size();
+	Error += test_texture3d_size();
 	Error += test_texture3d_query();
 	Error += test_texture3d_clear();
-	Error += test_texture3d_image_access();
+	Error += test_texture3d_access();
 
 	return Error;
 }
